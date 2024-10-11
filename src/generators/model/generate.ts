b3e52141969e8d69ce4a541c0a5550541c2b256c
mikana0918 <shogo.kusuhara@gmail.com>
2024-10-11 19:22:30 +0900

    Refactor generator output handling and remove unused code

--- src/generators/model/generate.ts
+++ src/generators/model/generate.ts
@@ -1,15 +1,8 @@
 import { EnvValue, GeneratorOptions } from "@prisma/generator-helper";
 import Transformer from "./transformer";
 import { parseEnvValue } from "@prisma/internals";
-import removeDir from "../utils/removeDir";
 
 export async function generate(options: GeneratorOptions) {
-  if (!options.generator.output) {
-    throw new Error(
-      `The 'output' field in the 'generator' section of the Prisma schema is required.`,
-    );
-  }
-
   try {
     const models = options.dmmf.datamodel.models;
 
@@ -17,20 +10,14 @@ export async function generate(options: GeneratorOptions) {
       models,
     });
 
-    const parsedPath = parseEnvValue(options.generator.output as EnvValue);
+    if (options.generator.output) {
+      const parsedPath = parseEnvValue(options.generator.output as EnvValue);
 
-    t.setOutputPath({ path: parsedPath });
-
-    await cascadeDeleteDirectory({
-      path: parsedPath,
-    });
+      t.setOutputPath({ path: parsedPath });
+    }
 
     await t.transform();
   } catch (e) {
     console.error(e);
   }
 }
-
-const cascadeDeleteDirectory = (args: { path: string }) => {
-  return removeDir(args.path, false);
-};
