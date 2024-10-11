cfadca1ff5904d415db9173055eb6b6b7e765b03
mikana0918 <shogo.kusuhara@gmail.com>
2024-10-11 18:29:58 +0900

    Refactor generator output handling and update package version

--- src/generators/model/index.ts
+++ src/generators/model/index.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-import { generatorHandler } from '@prisma/generator-helper';
-import { generate as generateDomainModel } from './generate';
+import { generatorHandler } from "@prisma/generator-helper";
+import { generate as generateDomainModel } from "./generate";
+import packageJson from "../../../package.json";
 
 generatorHandler({
   onManifest() {
     return {
-      defaultOutput: '__generated__/models',
-      prettyName: '[Frourio Framework]Models',
-      version: '1.0.0',
+      defaultOutput: "__generated__/models",
+      prettyName: "[Frourio Framework]Models",
+      version: packageJson.version,
     };
   },
   onGenerate: generateDomainModel,
