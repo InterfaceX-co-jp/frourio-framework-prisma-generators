2bfcb9db8495c8e564630b2f03610dea3dac4b42
mikana0918 <shogo.kusuhara@gmail.com>
2024-11-21 04:02:09 +0900

    feat: bump version to 1.1.0 and update package.sh for file copying

--- package.sh
+++ package.sh
@@ -9,7 +9,7 @@ mkdir package
 
 echo "Copying files..."
 cp -r lib package/lib
-cp package.json README.md LICENSE package
+cp package.json README.md package
 
 echo "Making package.json public..."
 sed -i 's/"private": true/"private": false/' ./package/package.json
